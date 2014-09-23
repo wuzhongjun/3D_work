96e30a6a485c2b9bf44b75648ac925ed4642e20e
wuzhongjun <1055820191@qq.com>
2014-09-23 18:52:18 +0800

    Revert "3D_work"
    This reverts commit c658527da9346ff42412cf8b91483877d8bcb465.

--- affi_change.m
+++
@@ -1,8 +0,0 @@
-function [Iwxp]=affi_change(pic, tmplt_pts, ref_point,lx,ly,rx,ry,mx,my)
-%tmplt_pts:图像边框四段坐标 eg: tmplt_pts=[1 1;1 150; 120 1;120
-%150]';如果要生成图像就需要，否则不需要
-
-
-    
-affm=maketform('affine', [lx ly;rx ry;mx my],ref_point);%投影变换结构
-Iwxp=imtransform(pic,affm,'XData',[tmplt_pts(1,1) tmplt_pts(1,4)],'YData',[tmplt_pts(2,1) tmplt_pts(2,4)],'FillValues',120);
--- lbp_histo.m
+++
@@ -1,8 +0,0 @@
-function out = lbp_histo(histvals,in)
-m = size(histvals,1);
-n = size(in,2);
-out = zeros(m,n);
-for i = 1:m
-   out(i,:) = sum(in == histvals(i));
-end
- 
